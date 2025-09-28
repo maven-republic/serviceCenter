ec538e25c80dd01e3f512b6c2d23bab46a8b042d
Rohan-R-Bedward <rohanbedward@gmail.com>
2025-09-28 12:41:56 -0500

    disable dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: "class",
+  darkMode: false,
   content: [
     // Scan ALL files - unified system
     './src/**/*.{js,ts,jsx,tsx,mdx}',
